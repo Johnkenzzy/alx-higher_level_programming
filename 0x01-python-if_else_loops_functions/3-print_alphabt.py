88605007fd50f929073ff3988c2f9807830b2830
johnkenzzy <johnkenumeh@gmail.com>
2024-09-10 21:07:49 -0700

    Task 3 corrected

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for a in range(97, 123):
-    if chr(a) == 'q' or chr(a) == 'o':
+    if chr(a) == 'e' or chr(a) == 'q':
         continue
     print("{}".format(chr(a)), end="")
