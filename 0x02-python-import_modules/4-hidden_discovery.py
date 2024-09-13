7a4b519e8022de6842759e73b58013b64349bfa1
johnkenzzy <johnkenumeh@gmail.com>
2024-09-12 20:47:18 -0700

    Completed task4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from hidden_4 import *
-mod_list = dir(hidden_4)
-list_len = len(mod_list)
+import hidden_4
 if __name__ == "__main__":
-    for i in range(list_len):
-        print(f"{mod_list[i]}")
+    mod_list = dir(hidden_4)
+    for mod_list in sorted(mod_list):
+        if mod_list[:2] != "__":
+            print(mod_list)
