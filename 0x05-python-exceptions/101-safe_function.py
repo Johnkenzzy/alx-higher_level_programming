09f3879dc3f68ced638c42dc398212068aa3ce64
johnkenzzy <johnkenumeh@gmail.com>
2024-09-30 21:20:58 -0700

    Import module<for stderr print>

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+from sys import stderr
+
+
 def safe_function(fct, *args):
     try:
         return (fct(*args))
     except Exception as exc:
-        print("Exception: {}".format(exc))
+        print("Exception: {}".format(exc), file=stderr)
         return (None)
