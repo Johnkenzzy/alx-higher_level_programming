4366290ffa9d7e906b186c9544e2e2f79e561aeb
johnkenzzy <johnkenumeh@gmail.com>
2024-10-01 21:50:17 -0700

    Task2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module contains a class square (based on 1-square.py)
+"""
+
+
+class Square:
+    """A shape with 4 equal sides."""
+
+    def __init__(self, size=0):
+        """Initializes the square with a given size.
+
+        Args:
+            size (int): The size of the square.
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
