2a7d57dd1565ef8848ec5b7a1d234ad74c71184b
johnkenzzy <johnkenumeh@gmail.com>
2024-10-07 10:42:47 -0700

    Completed task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is the 0-rectangle module
+
+This module provides the Rectangle class
+"""
+
+
+class Rectangle:
+    """
+    This is the rectangle class, it contains nothing
+    """
+    pass
