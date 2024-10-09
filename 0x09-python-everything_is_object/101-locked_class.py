41fa531725bf995677fd08715713b69ffa1ce660
johnkenzzy <johnkenumeh@gmail.com>
2024-10-08 19:26:29 -0700

    Advanced task 2 fix

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,15 @@
+#!/usr/bin/python3
+"""
+Module 101-locked_class
+
+Supplies the LockedClass class
+"""
+
+
 class LockedClass:
     """limit the creation of new attributes to first_name"""
     __slots__ = ['first_name']
 
     def __init__(self, first_name=None):
+        """Inntializes the instance attribute"""
         self.first_name = first_name
