1f8db6404184553d6f67892ad4d182c14994b0a1
johnkenzzy <johnkenumeh@gmail.com>
2024-10-14 00:35:31 -0700

    Partial fix

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -55,5 +55,5 @@ def lazy_matrix_mul(m_a, m_b):
 
     l1 = np.array(m_a)
     l2 = np.array(m_b)
-    result = l1 @ l2 #np.dot(l1, l2)
-    return (result.tolist())
+    result = l1 @ l2  # np.dot(l1, l2)
+    return (result)
