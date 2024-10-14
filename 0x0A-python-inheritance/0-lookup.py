bf84e272688257867471c75c4b49830ed485110f
johnkenzzy <johnkenumeh@gmail.com>
2024-10-14 08:56:23 -0700

    Completed task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This is module 0-lookup
+This modules supplies the lookup(obj)
+"""
+
+
+def lookup(obj):
+    """Returns the list of all available attributes and methods of an object"""
+    return (dir(obj))
