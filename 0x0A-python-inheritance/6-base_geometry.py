3757e59b5895014e90bd321a27706acaa004d1a5
johnkenzzy <johnkenumeh@gmail.com>
2024-10-14 19:39:31 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry (based on 5-base_geometry)
+Defines the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """A class of geometrical shapes"""
+    def area(self):
+        """the measure of the parameters of an object"""
+        raise Exception("area() is not implemented")
