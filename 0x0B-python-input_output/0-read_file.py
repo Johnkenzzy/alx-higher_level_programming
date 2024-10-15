0c830da6a431ecabc5df42cb1f01af6e80249d92
johnkenzzy <johnkenumeh@gmail.com>
2024-10-15 16:26:31 -0700

    Completed task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module 0-read_file
+Contains read_file(filename="")
+"""
+
+
+def read_file(filename=""):
+    """Reads a file's content and prints it to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
