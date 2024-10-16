0bee4441623d8526726c90e2347e887a2215d23d
johnkenzzy <johnkenumeh@gmail.com>
2024-10-15 21:23:36 -0700

    Completed task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module 10-student
+Defines the class Student (based on 9-studenit)
+"""
+
+
+class Student:
+    """The class of Students"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize instance attributes of Student class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return (self.__dict__)
+        else:
+            t = attrs
+            return ({t: self.__dict__[t] for t in t if t in self.__dict__})
