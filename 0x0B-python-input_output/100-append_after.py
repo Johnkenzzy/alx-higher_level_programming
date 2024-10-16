24c67de1bda9fdf3fbdb6c8fcd22276dc4cb02e9
johnkenzzy <johnkenumeh@gmail.com>
2024-10-15 21:58:31 -0700

    Advanced task 1

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 100-append_after
+Defines append_after(filename="", search_string="", new_string="")
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts a line of text after each line containing a specific string"""
+    with open(filename, 'r', encoding="utf-8") as f:
+        lines = f.readlines()
+
+    with open(filename, 'a', encoding="utf-8") as f:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string)
