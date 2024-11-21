e4a0db84640f45d4269bf38e65444b5c960a20a0
johnkenzzy <johnkenumeh@gmail.com>
2024-11-20 20:43:00 -0800

    Correction for task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,21 +1,9 @@
 -- Lists all the cities of California that can be found in the database hbtn_0d_usa.
 
--- Create the database if it does not already exist
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
--- Use the created or existing database
-USE hbtn_0d_usa;
-
--- Create the table states if it does not already exist
-CREATE TABLE IF NOT EXISTS states (
-    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
-);
-
--- Create the table cities if it does not already exist
-CREATE TABLE IF NOT EXISTS cities (
-    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id) ON DELETE CASCADE ON UPDATE CASCADE
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = (
+    SELECT states.id
+    FROM states
+    WHERE states.name = 'California'
 );
