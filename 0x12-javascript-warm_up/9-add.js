220a3e772166d4a18ec2bc39746244e93f0c708f
johnkenzzy <johnkenumeh@gmail.com>
2024-12-17 05:25:47 +0100

    Coompleted task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function add(a, b) {
+  return a + b;
+}
+
+const firstInt = parseInt(process.argv[2]);
+const secondInt = parseInt(process.argv[3]);
+
+if (isNaN(firstInt) || isNaN(secondInt)) {
+  console.log('NaN');
+} else {
+  console.log(add(firstInt, secondInt));
+}
