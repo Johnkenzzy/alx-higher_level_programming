8b93d9297ad78b6ba35bfae234483889e2702422
johnkenzzy <johnkenumeh@gmail.com>
2024-12-17 04:33:20 +0100

    Code optimization

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 
-if (process.argv.length < 3) {
+argc = process.argv.length - 2;
+
+if (argc === 0) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
+} else if (argc === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
