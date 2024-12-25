5732241a2fd1dadaf0eba4963b78b875e3f25ba4
johnkenzzy <johnkenumeh@gmail.com>
2024-12-26 00:24:35 +0100

    Script import execution guard

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,15 +5,16 @@ Lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
-arg_list = sys.argv[1:]
-conn = MySQLdb.connect(
+if __name__ == "__main__":
+    arg_list = sys.argv[1:]
+    conn = MySQLdb.connect(
         host="localhost", port=3306, user=arg_list[0],
         passwd=arg_list[1], db=arg_list[2], charset="utf8"
         )
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-conn.close()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
