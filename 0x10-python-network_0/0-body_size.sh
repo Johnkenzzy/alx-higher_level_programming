894dd3363e837fa3434890544a44518e722b362c
johnkenzzy <johnkenumeh@gmail.com>
2025-01-17 02:29:39 +0100

    Conform to requirements

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Sends a request to s URL, and displays the size of the body of the response
-
-if [ "$#" -ne 1 ]; then
-    exit 1
-fi
-
-URL=$1
-size=$(curl -s "$URL" | wc -c)
-
-echo "$size"
+echo $(curl -s "$1" | wc -c)
