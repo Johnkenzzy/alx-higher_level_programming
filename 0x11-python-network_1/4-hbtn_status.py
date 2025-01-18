daad4cacd1492a65978502a2e890226447b2b494
Johnkennedy Umeh <126101340+Johnkenzzy@users.noreply.github.com>
2025-01-18 04:26:39 +0100

    variable name

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,8 +6,8 @@ import requests
 
 
 url = 'https://alx-intranet.hbtn.io/status'
-req = requests.get(url)
+resp = requests.get(url)
 
 print('Body response:')
-print(f'\t- type: {type(req.text)}')
-print(f'\t- content: {req.text}')
+print(f'\t- type: {type(resp.text)}')
+print(f'\t- content: {resp.text}')
