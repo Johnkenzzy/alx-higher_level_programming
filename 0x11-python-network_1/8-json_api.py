c15edbd1671f4747d1eefb29005fafc9be81a331
johnkenzzy <johnkenumeh@gmail.com>
2025-01-17 20:24:32 -0800

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This script sends a POST request to http://0.0.0.0:5000/search_user
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_useri'
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+
+    data = {'q': q}
+
+    try:
+        resp = requests.post(url, data=data)
+        resp_json = resp.json()
+
+        if resp_json:
+            id = resp_json.get("id", None)
+            name = resp_json.get("name", None)
+            print(f'[{id}] {name}')
+        else:
+            print(f'No result')
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
