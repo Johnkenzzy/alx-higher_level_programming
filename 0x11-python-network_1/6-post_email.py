4c40fd1b4ee6fea4384e9aeadc70fd62c6ad2a61
johnkenzzy <johnkenumeh@gmail.com>
2025-01-17 19:36:02 -0800

    Fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     email = sys.argv[2]
     payload = {'email': email}
 
-    resp = request.post(url, data=payload)
+    resp = requests.post(url, data=payload)
     print(f'{resp.text}')
